05da02f027afcab0e126d7de433b50907bc5fef0
mdav0032 <89821849+mdav0032@users.noreply.github.com>
2022-10-10 16:51:17 +1100

    Added js files

---
+++ js_files/vega_lite_vis.js
@@ -0,0 +1,24 @@
+var one_vg = "./json_files/1_arrivals_and_departures_by_country_and_year_map.vg.json"; 
+vegaEmbed("#arrivals_and_departures_map", one_vg).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var two_vg = "./json_files/2_receipts_vs_expenditure_by_country_bar_chart.vg.json"; 
+vegaEmbed("#receipts_and_expenditure_bar", two_vg).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var three_vg = "./json_files/3_arrivals_and_departures_by_continent_treemap.vg.json"; 
+vegaEmbed("#arrivals_and_departures_treemap", three_vg).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var four_vg = "./json_files/4_attractions_by_continent_and_country_dendrogram.vg.json"; 
+vegaEmbed("#attractions_dendrogram", four_vg).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var five_vg = "./json_files/5_aus_traffic_by_airport_map.vg.json"; 
+vegaEmbed("#airport_traffic_map", five_vg).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
